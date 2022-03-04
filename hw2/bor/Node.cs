6a187b643a5e2cfdea31c5ce99d01b88cdeeb57c
kleo-53 <liza.1610@mail.ru>
2022-03-04 23:05:06 +0300

    реализовала бор

---
+++ hw2/bor/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace bor
+{
+    class Node
+    {
+        public Node?[] Children;
+        public int HowManyWordsPassedNode;
+        public int HowManyWordsEndsHere;
+        public Node()
+        {
+            this.Children = new Node[26];
+            this.HowManyWordsPassedNode = 0;
+            this.HowManyWordsEndsHere = 0;
+        }
+    }
+}
