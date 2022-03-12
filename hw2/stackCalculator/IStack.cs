ea7dccc44034d229cc7e14a7563f098d3ecdfd8e
kleo-53 <liza.1610@mail.ru>
2022-03-12 19:03:30 +0300

    реализовала стековый калькулятор

---
+++ hw2/stackCalculator/IStack.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StackCalculator.Tests
+{
+    internal interface IStack
+    {
+        /// <summary>
+        /// Добавляет элемент в стек
+        /// </summary>
+        /// <param name="element">Добавляемый элемент</param>
+        void Push(double element);
+        /// <summary>
+        /// Удаляет и возвращает верхний элемент стека
+        /// </summary>
+        /// <returns>Удаленный элемент</returns>
+        double Pop();
+        int Count();
+    }
+}
---
+++ hw2/stackCalculator/StackOnList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace StackCalculator.Tests;
+
+internal class StackOnList : IStack
+{
+
+    private List<double> stack;
+    private const int stackSize = 10;
+    private int count;
+    public StackOnList()
+    {
+        stack = new List<double>();
+    }
+    // Добавляет элемент
+    public void Push(double element)
+    {
+        stack.Insert(0, element);
+        ++count;
+    }
+    // Возвращает, пустой ли стек
+    public bool IsEmpty
+    {
+        get { return count == 0; }
+    }
+    // Возвращает размер стека
+    public int Count()
+    {
+        return count;
+    }
+    // Удаляет и возвращает первый элемент стека
+    public double Pop()
+    {
+        if (IsEmpty)
+        {
+            throw new InvalidOperationException("Стек пуст");
+        }
+        double element = stack.First();
+        stack.RemoveAt(0);
+        --count;
+        return element;
+    }
+}
