4db8b30d51f14d70ecc7729c6aeaea8d1f5c0220
kleo-53 <liza.1610@mail.ru>
2022-03-13 15:56:16 +0300

    tried to to some tests

--- hw2/stackCalculator.Tests/StackTest.cs
+++ hw2/stackCalculator.Tests/StackTest.cs
@@ -1,5 +1,5 @@
 using NUnit.Framework;
-using stackCalculator;
+using StackCalculator;
 
 namespace stackCalculator.Tests
 {
@@ -8,15 +8,63 @@ namespace stackCalculator.Tests
         [SetUp]
         public void Setup()
         {
+            IStack stack = new StackOnArray();
         }
 
+        /// <summary>
+        /// Проверка функции добавления элемента у стека на массиве
+        /// </summary>
         [Test]
-        public void PushTest()
+        public void PushToAtackOnArrayTest()
         {
             var stack = new StackOnArray();
-            Assert.IsTrue(stack.IsEmpty());
+            Assert.IsTrue(stack.IsEmpty);
             stack.Push(1);
-            Assert.IsFalse(stack.IsEmpty());
+            Assert.IsFalse(stack.IsEmpty);
+        }
+
+        /// <summary>
+        /// Проверка функции добавления элемента у стека на списке
+        /// </summary>
+        [Test]
+        public void PushToAtackOnAListTest()
+        {
+            var stack = new StackOnList();
+            Assert.IsTrue(stack.IsEmpty);
+            stack.Push(1);
+            Assert.IsFalse(stack.IsEmpty);
+        }
+
+        /// <summary>
+        /// Проверка функции удаления двух элементов из стека на массиве
+        /// </summary>
+        [Test]
+        public void PopTestWithTwoElementsFromStackOnArray()
+        {
+            var stack = new StackOnArray();
+            stack.Push(1);
+            stack.Push(2);
+            Assert.AreEqual(2, stack.Pop());
+            Assert.AreEqual(1, stack.Pop());
+        }
+
+        /// <summary>
+        /// Проверка функции удаления двух элементов из стека на списке
+        /// </summary>
+        [Test]
+        public void PopTestWithTwoElementsFromStackOnList()
+        {
+            var stack = new StackOnList();
+            stack.Push(1);
+            stack.Push(2);
+            Assert.AreEqual(2, stack.Pop());
+            Assert.AreEqual(1, stack.Pop());
+        }
+        [Test]
+        public void PopTestWithNoPush()
+        {
+            var stack = new StackOnList();
+            Assert.AreEqual(null, stack.Pop());
         }
 
     }
