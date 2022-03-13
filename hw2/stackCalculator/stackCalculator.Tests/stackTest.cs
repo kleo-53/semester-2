3bd6dac1ffbe9cf4a5ca93d4f6ac6d99b6acf2dc
kleo-53 <liza.1610@mail.ru>
2022-03-13 12:55:44 +0300

    tried to add test module

---
+++ hw2/stackCalculator/stackCalculator.Tests/stackTest.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+using StackCalculator;
+
+namespace stackCalculator.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void PushTest()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void PopTest()
+        {
+            var stack = new StackOnArray();
+            stack.Push(1);
+            var result = stack.Pop();
+            Assert.AreEqual(1, result);
+        }
+
+    }
+}
