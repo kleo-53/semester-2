86c80030a7495c193b99168059281940177fe9aa
kleo-53 <liza.1610@mail.ru>
2022-04-14 12:46:37 +0300

    fixed bugs

--- hw1/arraySort/Program.cs
+++ hw1/arraySort/Program.cs
@@ -1,47 +1,57 @@
-﻿using System;
-namespace arraySort
-{
-    static class Program
-    {
-        private static void Swap(ref int first, ref int second)
-        {
-            (first, second) = (second, first);
-        }
+﻿namespace ArraySort;
 
-        static void BubbleSort(int[] sortingArray)
+using System;
+
+/// <summary>
+/// Сортировка массива сортировкой пузырьком
+/// </summary>
+static class Program
+{
+    /// <summary>
+    /// Меняет два элемента местами
+    /// </summary>
+    /// <param name="first">Первый элемент</param>
+    /// <param name="second">Второй элемент</param>
+    private static void Swap(ref int first, ref int second)
+    {
+        (first, second) = (second, first);
+    }
+
+    /// <summary>
+    /// Сортирует массив сортировкой пузырьком
+    /// </summary>
+    /// <param name="sortingArray">Массив, который надо отсортировать</param>
+    static void BubbleSort(int[] sortingArray)
+    {
+        var length = sortingArray.Length;
+        for (var i = 1; i < length; i++)
         {
-            var length = sortingArray.Length;
-            for (var i = 1; i < length; i++)
+            for (var j = 0; j < length - i; j++)
             {
-                for (var j = 0; j < length - i; j++)
+                if (sortingArray[j] > sortingArray[j + 1])
                 {
-                    if (sortingArray[j] > sortingArray[j + 1])
-                    {
-                        Swap(ref sortingArray[j], ref sortingArray[j + 1]);
-                    }
+                    Swap(ref sortingArray[j], ref sortingArray[j + 1]);
                 }
             }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите элементы массива для сортировки пузырьком:");
+        var inputString = Console.ReadLine();
+        if (inputString == null)
+        {
+            Console.WriteLine("Введен пустой массив!");
             return;
         }
-
-        static void Main(string[] args)
+        var arrayElements = inputString.Split(' ');
+        var inputArray = new int[arrayElements.Length];
+        for (var i = 0; i < arrayElements.Length; i++)
         {
-            Console.WriteLine("Введите элементы массива для сортировки пузырьком:");
-            var inputString = Console.ReadLine();
-            if (inputString == null)
-            {
-                Console.WriteLine("Введен пустой массив!");
-                return;
-            }
-            var arrayElements = inputString.Split(' ');
-            var inputArray = new int[arrayElements.Length];
-            for (var i = 0; i < arrayElements.Length; i++)
-            {
-                inputArray[i] = Convert.ToInt32(arrayElements[i]);
-            }
-            BubbleSort(inputArray);
-            Console.WriteLine($"Отсортированный массив: {string.Join(' ', inputArray)}");
-
+            inputArray[i] = Convert.ToInt32(arrayElements[i]);
         }
+        BubbleSort(inputArray);
+        Console.WriteLine($"Отсортированный массив: {string.Join(' ', inputArray)}");
     }
 }
