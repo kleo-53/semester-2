2ea1a1d335d046fa41e2a4c4d2c476436a15868f
kleo-53 <liza.1610@mail.ru>
2022-05-31 14:22:06 +0300

    fixed tests

--- hw2/stack.Tests/stackTest.cs
+++ hw2/stack.Tests/stackTest.cs
@@ -3,68 +3,62 @@ using StackCalculator;
 using System.Collections.Generic;
 using System;
 
-namespace stack.Tests
+namespace stack.Tests;
+
+public class Tests
 {
-    public class Tests
+    private static IEnumerable<IStack> StackToTest
     {
-        [SetUp]
-        public void Setup()
+        get
         {
+            yield return new StackOnArray();
+            yield return new StackOnList();
         }
+    }
 
-        private static IEnumerable<IStack> StackToTest
-        {
-            get
-            {
-                yield return new StackOnArray();
-                yield return new StackOnList();
-            }
-        }
+    /// <summary>
+    /// Проверка функции добавления элемента в стек и корректной работы функции IsEmpty
+    /// </summary>
+    [TestCaseSource(nameof(StackToTest))]
+    public void PushToStackWorksCorrectlyTest(IStack stack)
+    {
+        Assert.IsTrue(stack.IsEmpty);
+        stack.Push(1);
+        Assert.IsFalse(stack.IsEmpty);
+    }
 
-        /// <summary>
-        /// Проверка функции добавления элемента в стек и корректной работы функции IsEmpty
-        /// </summary>
-        [Test, TestCaseSource(nameof(StackToTest))]
-        public void PushToStackWorksCorrectlyTest(IStack stack)
-        {
-            Assert.IsTrue(stack.IsEmpty);
-            stack.Push(1);
-            Assert.IsFalse(stack.IsEmpty);
-        }
+    /// <summary>
+    /// Проверка функции IsEmpty после добавления и удаления элемента из стека
+    /// </summary>
+    [Test, TestCaseSource(nameof(StackToTest))]
+    public void StackIsEmptyAfterPushAndPopTest(IStack stack)
+    {
+        Assert.IsTrue(stack.IsEmpty);
+        stack.Push(1);
+        stack.Pop();
+        Assert.IsTrue(stack.IsEmpty);
+    }
 
-        /// <summary>
-        /// Проверка функции IsEmpty после добавления и удаления элемента из стека
-        /// </summary>
-        [Test, TestCaseSource(nameof(StackToTest))]
-        public void StackIsEmptyAfterPushAndPopTest(IStack stack)
-        {
-            Assert.IsTrue(stack.IsEmpty);
-            stack.Push(1);
-            stack.Pop();
-            Assert.IsTrue(stack.IsEmpty);
-        }
+    /// <summary>
+    /// Проверка работы принципа First-In-Last-Out для трех элементов
+    /// </summary>
+    [Test, TestCaseSource(nameof(StackToTest))]
+    public void PopWithThreeElementsFromStackWorksCorrectlyTest(IStack stack)
+    {
+        stack.Push(1);
+        stack.Push(3);
+        stack.Push(2);
+        Assert.AreEqual(2, stack.Pop().Item1);
+        Assert.AreEqual(3, stack.Pop().Item1);
+        Assert.AreEqual(1, stack.Pop().Item1);
+    }
 
-        /// <summary>
-        /// Проверка работы принципа First-In-Last-Out для трех элементов
-        /// </summary>
-        [Test, TestCaseSource(nameof(StackToTest))]
-        public void PopWithThreeElementsFromStackWorksCorrectlyTest(IStack stack)
-        {
-            stack.Push(1);
-            stack.Push(3);
-            stack.Push(2);
-            Assert.AreEqual(2, stack.Pop().Item1);
-            Assert.AreEqual(3, stack.Pop().Item1);
-            Assert.AreEqual(1, stack.Pop().Item1);
-        }
-
-        /// <summary>
-        /// Проверка появления ошибки при попытке удалить элемент из пустого стека
-        /// </summary>
-        [Test, TestCaseSource(nameof(StackToTest))]
-        public void PopWithNoPushThrowsExceptionTest(IStack stack)
-        {
-            Assert.IsFalse(stack.Pop().Item2);
-        }
+    /// <summary>
+    /// Проверка появления ошибки при попытке удалить элемент из пустого стека
+    /// </summary>
+    [Test, TestCaseSource(nameof(StackToTest))]
+    public void PopWithNoPushThrowsExceptionTest(IStack stack)
+    {
+        Assert.IsFalse(stack.Pop().Item2);
     }
 }
--- hw2/stackCalculator.Tests/StackCalculatorTest.cs
+++ hw2/stackCalculator.Tests/StackCalculatorTest.cs
@@ -7,11 +7,6 @@ using System;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
     private static IEnumerable<IStack> StackToTest
     {
         get
@@ -21,18 +16,16 @@ public class Tests
         }
     }
 
-    private const double epsilon = 1e-7;
-
     /// <summary>
     /// Проверка работы каждой операции на корректных тестах
     /// </summary>
     [Test, TestCaseSource(nameof(StackToTest))]
     public void EachCorrectOperationWorksCorrectlyTest(IStack stack)
     {
-        Assert.IsTrue(Calculator.Calculation("132", stack).Item1 - 132 <= epsilon);
-        Assert.IsTrue(Calculator.Calculation("-12", stack).Item1 + 12 <= epsilon);
-        Assert.IsTrue(Calculator.Calculation("1 2 45 * - 14 + 2 - 3 /", stack).Item1 + 25.66666 <= epsilon);
-        Assert.IsTrue(Calculator.Calculation("20 17 - 34 59 * + 74 - 1 130 / + 81 +", stack).Item1 - 2016.0076923 <= epsilon);
+        Assert.AreEqual(Calculator.Calculation("132", stack).Item1, 132);
+        Assert.AreEqual(Calculator.Calculation("-12", stack).Item1, -12);
+        Assert.AreEqual(Calculator.Calculation("1 2 45 * - 14 + 2 - 3 /", stack).Item1, -25.666666666666668);
+        Assert.AreEqual(Calculator.Calculation("20 17 - 34 59 * + 74 - 1 130 / + 81 +", stack).Item1, 2016.0076923076922);
     }
 
     /// <summary>
@@ -41,13 +34,13 @@ public class Tests
     [Test, TestCaseSource(nameof(StackToTest))]
     public void ExpressionsWithZeroTest(IStack stack)
     {
-        Assert.IsTrue(Calculator.Calculation("5 0 +", stack).Item1 - 5 <= epsilon);
-        Assert.IsTrue(Calculator.Calculation("0 47 +", stack).Item1 - 47 <= epsilon);
-        Assert.IsTrue(Calculator.Calculation("0 2 -", stack).Item1 + 2 <= epsilon);
-        Assert.IsTrue(Calculator.Calculation("4 0 -", stack).Item1 - 4 <= epsilon);
-        Assert.IsTrue(Calculator.Calculation("4 0 *", stack).Item1 + 0 <= epsilon);
-        Assert.IsTrue(Calculator.Calculation("0 -5 *", stack).Item1 + 0 <= epsilon);
-        Assert.IsTrue(Calculator.Calculation("0 6 /", stack).Item1 + 0 <= epsilon);
+        Assert.AreEqual(Calculator.Calculation("5 0 +", stack).Item1, 5);
+        Assert.AreEqual(Calculator.Calculation("0 47 +", stack).Item1, 47);
+        Assert.AreEqual(Calculator.Calculation("0 2 -", stack).Item1, -2);
+        Assert.AreEqual(Calculator.Calculation("4 0 -", stack).Item1, 4);
+        Assert.AreEqual(Calculator.Calculation("4 0 *", stack).Item1, 0);
+        Assert.AreEqual(Calculator.Calculation("0 -5 *", stack).Item1, 0);
+        Assert.AreEqual(Calculator.Calculation("0 6 /", stack).Item1, 0);
     }
 
     /// <summary>
@@ -56,7 +49,7 @@ public class Tests
     [Test, TestCaseSource(nameof(StackToTest))]
     public void ExceptionInDivisionByZeroTest(IStack stack)
     {
-        Assert.Throws<DivideByZeroException>(() => { Calculator.Calculation("34 9287 + 123 4 * - 0 /", stack); });
+        Assert.Throws<DivideByZeroException>(() => Calculator.Calculation("34 9287 + 123 4 * - 0 /", stack));
     }
 
     /// <summary>
@@ -65,10 +58,10 @@ public class Tests
     [Test, TestCaseSource(nameof(StackToTest))]
     public void NegativeNumbersInStackTest(IStack stack)
     {
-        Assert.IsTrue(Calculator.Calculation("-5 -4 +", stack).Item1 + 1 <= epsilon);
-        Assert.IsTrue(Calculator.Calculation("-1 -17 -", stack).Item1 - 16 <= epsilon);
-        Assert.IsTrue(Calculator.Calculation("-6 -3 *", stack).Item1 - 18 <= epsilon);
-        Assert.IsTrue(Calculator.Calculation("-1024 -2 -", stack).Item1 - 512 <= epsilon);
+        Assert.AreEqual(Calculator.Calculation("-5 -4 +", stack).Item1, -9);
+        Assert.AreEqual(Calculator.Calculation("-1 -17 -", stack).Item1, 16);
+        Assert.AreEqual(Calculator.Calculation("-6 -3 *", stack).Item1, 18);
+        Assert.AreEqual(Calculator.Calculation("-1024 -2 -", stack).Item1, -1022);
     }
 
     /// <summary>
@@ -79,7 +72,7 @@ public class Tests
     {
         Assert.IsFalse(Calculator.Calculation("5 45647", stack).Item2);
         Assert.IsFalse(Calculator.Calculation("5 45647 - +", stack).Item2);
-        Assert.Throws<IndexOutOfRangeException>(() => { Calculator.Calculation("", stack); } );
+        Assert.Throws<IndexOutOfRangeException>(() => Calculator.Calculation("", stack));
         Assert.IsFalse(Calculator.Calculation("ww vfd +", stack).Item2);
         Assert.IsFalse(Calculator.Calculation("/", stack).Item2);
         Assert.IsFalse(Calculator.Calculation("1 * 2", stack).Item2);
