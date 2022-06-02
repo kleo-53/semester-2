883920ad380bdef4920bcf48ee18d12f856e1906
kleo-53 <liza.1610@mail.ru>
2022-06-02 20:17:00 +0300

    last fixes

--- hw6/map filter fold/MapFilterFold.Tests/MapFilterFoldTest.cs
+++ hw6/map filter fold/MapFilterFold.Tests/MapFilterFoldTest.cs
@@ -1,25 +1,44 @@
+namespace MapFilterFold.Tests;
+
 using NUnit.Framework;
 using MapFilterFold;
 using System.Collections.Generic;
 
-namespace MapFilterFold.Tests;
-
 public class Test
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
+    [Test]
+    public void MapTestMultiply() => Assert.AreEqual(MapFilterFoldFunctions.Map(new List<int>
+        { 1, -2, 3, 0 }, x => x * 4), new List<int> { 4, -8, 12, 0 });
 
     [Test]
-    public void MapTest() => Assert.AreEqual(MapFilterFoldFunctions.Map(new List<int>
-    { 1, -2, 3, 0 }, x => x * 4), new List<int> { 4, -8, 12, 0 });
+    public void MapTestSquare() => Assert.AreEqual(MapFilterFoldFunctions.Map(new List<int>
+        { 1, 2, 3, 4 }, x => x * x), new List<int>() { 1, 4, 9, 16 });
 
     [Test]
-    public void FilterTest() => Assert.AreEqual(MapFilterFoldFunctions.Filter(new List<int>
-    { 1, -2, 3, 0 }, x => x % 3 == 1), new List<int> { 1 });
+    public void MapEmptySquare() => Assert.AreEqual(MapFilterFoldFunctions.Map(new List<int>
+        { }, x => x * 2432), new List<int>() { });
 
     [Test]
-    public void FoldTest() => Assert.AreEqual(MapFilterFoldFunctions.Fold(new List<int>
-    { 1, -2, 3, 0 }, -8, (x, y) => x + 2 * y), -4);
+    public void FilterTestDivision() => Assert.AreEqual(MapFilterFoldFunctions.Filter(new List<int>
+        { 1, -2, 3, 0 }, x => x % 3 == 1), new List<int> { 1 });
+
+    [Test]
+    public void FilterInequalityTest() => Assert.AreEqual(MapFilterFoldFunctions.Filter(new List<int>
+        { 1, 2, 3, 4, 5, 10}, x => x < 3), new List<int>() { 1, 2 });
+
+    [Test]
+    public void FilterEmptyTest() => Assert.AreEqual(MapFilterFoldFunctions.Filter(new List<int>
+        { }, x => x > 0), new List<int>() { });
+
+    [Test]
+    public void FoldMultiplyTest() => Assert.AreEqual(MapFilterFoldFunctions.Fold(new List<int>
+        { 1, -2, 3, 0 }, -8, (x, y) => x + 2 * y), -4);
+
+    [Test]
+    public void FoldSubtractionTest() => Assert.AreEqual(MapFilterFoldFunctions.Fold(new List<int>
+        { 1, 2, 3, 4 }, 1, (x, y) => x - y), -9);
+
+    [Test]
+    public void FoldEmptyTest() => Assert.AreEqual(MapFilterFoldFunctions.Fold(new List<int>
+        { }, 1, (x, y) => x * y), 1);
 }
--- hw6/map filter fold/MapFilterFold/MapFilterFoldFunctions.cs
+++ hw6/map filter fold/MapFilterFold/MapFilterFoldFunctions.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿namespace MapFilterFold;
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MapFilterFold;
-
+/// <summary>
+/// Класс с функциями map, filter, fold
+/// </summary>
 public class MapFilterFoldFunctions
 {
     /// <summary>
@@ -16,7 +16,7 @@ public class MapFilterFoldFunctions
     /// <returns>Список из элементов, к каждому из которых применили функцию</returns>
     public static List<int> Map(List<int> list, Func<int, int> function)
     {
-        List<int> result = new List<int>();
+        var result = new List<int>();
         for (int i = 0; i < list.Count; i++)
         {
             result.Add(function(list[i]));
@@ -32,7 +32,7 @@ public class MapFilterFoldFunctions
     /// <returns>Список из элементов, для которых ппереданная функция истинна</returns>
     public static List<int> Filter(List<int> list, Func<int, bool> function)
     {
-        List<int> result = new List<int>();
+        var result = new List<int>();
         for (int i = 0; i < list.Count; i++)
         {
             if (function(list[i]))
