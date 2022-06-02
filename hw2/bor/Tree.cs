693d8aabd562690d99c1ae640a3383ebc3a1e5f6
kleo-53 <liza.1610@mail.ru>
2022-06-02 12:00:34 +0300

    add comments

--- hw2/bor/Tree.cs
+++ hw2/bor/Tree.cs
@@ -1,100 +1,150 @@
-﻿using System;
+﻿namespace bor;
 
-namespace bor
+using System;
+
+/// <summary>
+/// Класс бора
+/// </summary>
+public class Tree
 {
-    class Tree
+    /// <summary>
+    /// Элемент бора
+    /// </summary>
+    private class Node
     {
-        private Node root;
-        public int Size;
-        public Tree()
+        public Node?[] Children;
+        public int HowManyWordsPassedNode;
+        public int HowManyWordsEndsHere;
+        public Node()
         {
-            root = new Node();
-            this.Size = 0;
+            this.Children = new Node[26];
+            this.HowManyWordsPassedNode = 0;
+            this.HowManyWordsEndsHere = 0;
         }
-        public bool Add(string element)
+    }
+    private Node root;
+
+    /// <summary>
+    /// Размер бора
+    /// </summary>
+    public int Size { get; set; }
+
+    /// <summary>
+    /// Конструктор бора
+    /// </summary>
+    public Tree()
+    {
+        root = new Node();
+        this.Size = 0;
+    }
+
+    /// <summary>
+    /// Добавление элемента в бор
+    /// </summary>
+    /// <param name="element">Строка, которую нужно добавить</param>
+    /// <returns>Находился ли такой же элемент в боре до этого добавления</returns>
+    /// <exception cref="ArgumentNullException">Ошибка в случае добавления пустой строки</exception>
+    public bool Add(string element)
+    {
+        if (element == null)
         {
-            if (element == null)
+            throw new ArgumentNullException("String can not be empty!");
+        }
+        ++Size;
+        Node? current = root;
+        var WasNotInBor = false;
+        foreach (var symbol in element)
+        {
+            if (current == null)
             {
-                throw new ArgumentNullException("String can not be empty!");
+                return false;
             }
-            ++Size;
-            Node? current = root;
-            var WasNotInBor = false;
-            foreach (var symbol in element)
+            ++current.HowManyWordsPassedNode;
+            var code = symbol - 'a';
+            if (current.Children[code] == null)
             {
-                if (current == null)
-                {
-                    return false;
-                }
-                ++current.HowManyWordsPassedNode;
-                var code = symbol - 'a';
-                if (current.Children[code] == null)
-                {
-                    current.Children[code] = new Node();
-                    WasNotInBor = true;
-                }
+                current.Children[code] = new Node();
+                WasNotInBor = true;
+            }
+            current = current.Children[code];
+        }
+        ++current.HowManyWordsEndsHere;
+        return WasNotInBor;
+    }
+
+    /// <summary>
+    /// Проверка на нахождение в боре данного элемента
+    /// </summary>
+    /// <param name="element">Строка для проверки</param>
+    /// <returns>Находится строка в боре или нет</returns>
+    public bool Contains(string element)
+    {
+        Node? current = root;
+        foreach (var symbol in element)
+        {
+            var code = symbol - 'a';
+            if (current == null || current.Children == null || current.Children[code] == null)
+            {
+                return false;
+            }
+            current = current.Children[code];
+        }
+        return current.HowManyWordsEndsHere != 0;
+    }
+
+    /// <summary>
+    /// Удаление строки из бора
+    /// </summary>
+    /// <param name="element">Строка для удаления</param>
+    /// <returns>Успешно ли удален элемент</returns>
+    public bool Remove(string element)
+    {
+        Node? current = root;
+        var shouldRemove = false;
+        foreach (var symbol in element)
+        {
+            var code = symbol - 'a';
+            if (current == null || current.Children[code] == null)
+            {
+                return false;
+            }
+            if (current.Children[code].HowManyWordsPassedNode > 1)
+            {
+                --current.Children[code].HowManyWordsPassedNode;
                 current = current.Children[code];
             }
-            ++current.HowManyWordsEndsHere;
-            return WasNotInBor;
-        }
-        public bool Contains(string element)
-        {
-            Node? current = root;
-            foreach (var symbol in element)
+            else
             {
-                var code = symbol - 'a';
-                if (current == null || current.Children == null || current.Children[code] == null)
+                if (!shouldRemove)
                 {
-                    return false;
+                    shouldRemove = true;
                 }
-                current = current.Children[code];
+                var temporary = current.Children[code];
+                current.Children[code] = null;
+                current = temporary;
             }
-            return current.HowManyWordsEndsHere != 0;
         }
-        public bool Remove(string element)
+        --Size;
+        return true;
+    }
+
+    /// <summary>
+    /// Поиск количества строк, начинающихся с заданного префикса
+    /// </summary>
+    /// <param name="prefix">Данный префикс</param>
+    /// <returns>Количество слов с таким же префиксом</returns>
+    public int HowManyStartsWithPrefix(string prefix)
+    {
+        Node? current = root;
+        foreach (var symbol in prefix)
         {
-            Node? current = root;
-            var shouldRemove = false;
-            foreach (var symbol in element)
+            var code = symbol - 'a';
+            if (current == null || current.Children == null || current.Children[code] == null)
             {
-                var code = symbol - 'a';
-                if (current == null || current.Children[code] == null)
-                {
-                    return false;
-                }
-                if (current.Children[code].HowManyWordsPassedNode > 1)
-                {
-                    --current.Children[code].HowManyWordsPassedNode;
-                    current = current.Children[code];
-                }
-                else
-                {
-                    if (!shouldRemove)
-                    {
-                        shouldRemove = true;
-                    }
-                    var temporary = current.Children[code];
-                    current.Children[code] = null;
-                    current = temporary;
-                }
+                return 0;
             }
-            --Size;
-            return true;
+            current = current.Children[code];
         }
-        public int HowManyStartsWithPrefix(string prefix)
-        {
-            Node? current = root;
-            foreach (var symbol in prefix)
-            {
-                var code = symbol - 'a';
-                if (current == null || current.Children == null || current.Children[code] == null)
-                {
-                    return 0;
-                }
-                current = current.Children[code];
-            }
-            return current.HowManyWordsPassedNode;
-        }
+        return current.HowManyWordsPassedNode;
     }
 }
